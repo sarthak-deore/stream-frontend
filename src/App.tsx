91a54299fe2866392d04997b4a0f4914b55b6209
Sarthak Deore <deore.1@iitj.ac.in>
2024-06-06 19:25:19 +0530

    commit1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <video
+        controls
+        height="540"
+        width="960"
+        src="http://localhost:3000/video"
+      ></video>
+    </div>
+  );
+};
+
+export default App;
